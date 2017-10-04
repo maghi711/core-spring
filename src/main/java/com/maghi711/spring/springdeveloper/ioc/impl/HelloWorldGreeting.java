7fae018e024384519617e7d86c2f24dec4b58717
Mahesh J <maghi711@gmail.com>
2017-10-04 17:58:21 +0530

    minor refactoring

--- src/main/java/com/maghi711/spring/springdeveloper/ioc/impl/HelloWorldGreeting.java
+++ src/main/java/com/maghi711/spring/springdeveloper/ioc/impl/HelloWorldGreeting.java
@@ -1,4 +1,19 @@
 package com.maghi711.spring.springdeveloper.ioc.impl;
 
-public class HelloWorldGreeting {
+import com.maghi711.spring.springdeveloper.ioc.Greeting;
+
+import java.util.Random;
+
+public class HelloWorldGreeting implements Greeting {
+
+    @Override
+    public String greet(String name) {
+        Random random = null;
+        try {
+            random = new Random();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Hello World!";
+    }
 }
